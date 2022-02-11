272b9c44c9bae0aeae02583eb25249cff9082a30
test@gmail.com <test@gmail.com>
2022-02-11 09:56:15 +0000

    init commit

---
+++ src/main/java/com/soj/principle/openclosed/Main.java
@@ -0,0 +1,13 @@
+package com.soj.principle.openclosed;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    static final Logger logger= LogManager.getLogger(Main.class.getName());
+    public static void main(String[] args){
+        logger.info("hello");
+
+
+    }
+}
